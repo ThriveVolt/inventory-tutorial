a31f72ed0d7dee67ab306c6a808cc1d81cee998e
ThriveVolt <thrivevolt@gmail.com>
2022-01-14 17:19:34 +0800

    add item tooltip

--- main.gd
+++ main.gd
@@ -3,28 +3,34 @@ extends Node2D
 onready var hotbar = $UI/Hotbar
 onready var inventory_menu = $UI/InventoryMenu
 onready var drag_preview = $UI/DragPreview
+onready var tooltip = $UI/Tooltip
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_menu"):
 		if inventory_menu.visible and drag_preview.dragged_item: return
 		hotbar.visible = !hotbar.visible
 		inventory_menu.visible = !inventory_menu.visible
+		hide_tooltip()
 
 func _ready():
 	for item_slot in get_tree().get_nodes_in_group("item_slot"):
 		var index = item_slot.get_index()
 		item_slot.connect("gui_input", self, "_on_ItemSlot_gui_input", [index])
+		item_slot.connect("mouse_entered", self, "show_tooltip", [index])
+		item_slot.connect("mouse_exited", self, "hide_tooltip")
 
 func _on_ItemSlot_gui_input(event, index):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
 			if inventory_menu.visible:
 				drag_item(index)
+				hide_tooltip()
 			elif hotbar.visible:
 				select_item(index)
 		elif event.button_index == BUTTON_RIGHT and event.pressed:
 			if inventory_menu.visible:
 				split_item(index)
+				hide_tooltip()
 
 func drag_item(index):
 	var inventory_item = Inventory.items[index]
@@ -60,3 +66,14 @@ func split_item(index):
 
 func select_item(index):
 	Inventory.set_selected(index)
+
+func show_tooltip(index):
+	var inventory_item = Inventory.items[index]
+	if inventory_item and !drag_preview.dragged_item:
+		tooltip.display_info(inventory_item)
+		tooltip.show()
+	else:
+		tooltip.hide()
+
+func hide_tooltip():
+	tooltip.hide()
