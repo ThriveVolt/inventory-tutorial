ce68ef2b50da49daa03f50fea7446b6f11f47888
ThriveVolt <thrivevolt@gmail.com>
2022-01-14 16:47:36 +0800

    add stack and split items

--- main.gd
+++ main.gd
@@ -20,7 +20,9 @@ func _on_ItemSlot_gui_input(event, index):
 		if event.button_index == BUTTON_LEFT and event.pressed:
 			if inventory_menu.visible:
 				drag_item(index)
-
+		elif event.button_index == BUTTON_RIGHT and event.pressed:
+			if inventory_menu.visible:
+				split_item(index)
 
 func drag_item(index):
 	var inventory_item = Inventory.items[index]
@@ -31,6 +33,25 @@ func drag_item(index):
 	# drop item
 	elif !inventory_item and dragged_item:
 		drag_preview.dragged_item = Inventory.set_item(index, dragged_item)
-	# swap items
 	elif inventory_item and dragged_item:
-		drag_preview.dragged_item = Inventory.set_item(index, dragged_item)
+		# stack item
+		if inventory_item.key == dragged_item.key and inventory_item.stackable:
+			Inventory.set_item_quantity(index, dragged_item.quantity)
+			drag_preview.dragged_item = {}
+		# swap items
+		else:
+			drag_preview.dragged_item = Inventory.set_item(index, dragged_item)
+
+func split_item(index):
+		var inventory_item = Inventory.items[index]
+		var dragged_item = drag_preview.dragged_item
+		if !inventory_item: return
+		var split_amount = ceil(inventory_item.quantity / 2)
+		if dragged_item and inventory_item.key == dragged_item.key:
+			drag_preview.dragged_item.quantity += split_amount
+			Inventory.set_item_quantity(index, -split_amount)
+		if !dragged_item:
+			var item = inventory_item.duplicate()
+			item.quantity = split_amount
+			drag_preview.dragged_item = item
+			Inventory.set_item_quantity(index, -split_amount)
