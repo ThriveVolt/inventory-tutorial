0f43a91dfb9983e9d20fe1be0c3b9b0b8612f110
ThriveVolt <thrivevolt@gmail.com>
2022-01-14 13:29:32 +0800

    add inventory

---
+++ inventory.gd
@@ -0,0 +1,31 @@
+extends Node
+
+signal items_changed(indexes)
+
+var cols = 9
+var rows = 3
+var slots = cols * rows
+var items = []
+
+func _ready():
+	for i in range(slots):
+		items.append({})
+
+func set_item(index, item):
+	var previous_item = items[index]
+	items[index] = item
+	emit_signal("items_changed", [index])
+	return previous_item
+
+func remove_item(index):
+	var previous_item = items[index].duplicate()
+	items[index].clear()
+	emit_signal("items_changed", [index])
+	return previous_item
+
+func set_item_quantity(index, amount):
+	items[index].quantity += amount
+	if items[index].quantity <= 0:
+		remove_item(index)
+	else:
+		emit_signal("items_changed", [index])
